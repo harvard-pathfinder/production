97ebb651f0b78321a58b2a8de3efee9c37d4ccb0
Robert Shaw <robertshaw@Roberts-MacBook-Pro-2.local>
2015-04-19 12:01:22 -0400

    don't update board for a bit going to east lunch then i will implement the code needed for move...

--- pathfinder/Board.swift
+++ pathfinder/Board.swift
@@ -17,5 +17,32 @@ class Board {
         return [BoardNode(x: 0, y: 0, elts: nil)]
     }
     
-
+//    func set (point p: (Int, Int), (bNode: BoardNode) -> () {
+//        defaultBoard[x].[y] =
+//    }
+//    func modify (point p: (Int, Int), function f: (BoardNode -> BoardNode)) {
+//        
+    }
 }
+
+
+//* Set a location in the world to contain a new list of objects. *)
+
+//let set ((x,y):int*int) (wos:world_object_i list) : unit =
+
+//world.(x).(y) <- wos
+
+//(** Modify a location in the world with value os to contain (f os). *)
+//48
+//let modify (p:int*int) (f:world_object_i list -> world_object_i list) : unit =
+//49
+//set p (f (get p))
+//50
+//51
+//(** Add an object to the list of world objects at a location. *)
+//52
+//let add (p:int*int) (w:world_object_i) : unit =
+//53
+//modify p (fun wos -> if List.mem w wos then wos else w::wos)
+//54
+//55
