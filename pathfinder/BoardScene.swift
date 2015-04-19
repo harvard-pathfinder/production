76ce4651e508796e6977461fe716b4376b949874
Christian O <christian.ondaatje@gmail.com>
2015-04-19 16:51:07 -0400

    Starting UI projection of backend code

---
+++ pathfinder/BoardScene.swift
@@ -0,0 +1,22 @@
+//
+//  BoardScene.swift
+//  pathfinder
+//
+//  Created by Tester on 4/19/15.
+//  Copyright (c) 2015 harvard-pathfinder. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class BoardScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+    }
+
+}
--- pathfinder/GameScene.swift
+++ pathfinder/GameScene.swift
@@ -5,40 +5,38 @@
 //  Created by Tester on 4/18/15.
 //  Copyright (c) 2015 harvard-pathfinder. All rights reserved.
 //
-
+import Foundation
 import SpriteKit
 
 class GameScene: SKScene {
+    let startButton = SKLabelNode(fontNamed:"Times New Roman")
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        startButton.text = "Start Pathfinder";
+        startButton.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        self.addChild(startButton)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        let a = BoardGenerator().defaultBoard()
-        print(a)
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+
+            // if the user touches the survival button, go to the survival explanation
+            if nodeAtPoint(location) == startButton {
+                
+                // create a new scene
+                var scene = BoardScene()
+                let skView = self.view as SKView!
+                scene.size = skView.bounds.size
+                
+                // go to the new scene
+                skView.presentScene(scene)
+            }
+        }
         
-//        for touch: AnyObject in touches {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-//        }
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
