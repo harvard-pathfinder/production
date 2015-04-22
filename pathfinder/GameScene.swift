f21e3a68a1c40365d2b53e4c19547d3a624f9267
Sean Coleman <seancoleman@college.harvard.edu>
2015-04-21 21:01:50 -0400

    changed touches began to work with 8.3

--- pathfinder/GameScene.swift
+++ pathfinder/GameScene.swift
@@ -18,7 +18,7 @@ class GameScene: SKScene {
         self.addChild(startButton)
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    override func touchesBegan(touches: Set <NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
