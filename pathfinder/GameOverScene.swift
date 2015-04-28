e6337c4d6de208c025f9cbb4809830ba9122f1ed
Sean Coleman <seancoleman@college.harvard.edu>
2015-04-28 03:04:41 -0400

    gameOverScene

---
+++ pathfinder/GameOverScene.swift
@@ -0,0 +1,48 @@
+//
+//  GameOverScene.swift
+//  pathfinder
+//
+//  Created by Sean Coleman on 4/27/15.
+//  Copyright (c) 2015 harvard-pathfinder. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GaameOverScene: SKScene {
+    let gameOver = SKLabelNode (fontNamed:"Times New Roman")
+    let playAgainButton = SKLabelNode(fontNamed:"Times New Roman")
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        gameOver.text = "Game Over"
+        gameOver.fontSize = 40
+        gameOver.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        playAgainButton.text = "Start Pathfinder";
+        playAgainButton.fontSize = 20
+        playAgainButton.position = CGPoint(x:CGRectGetMidX(self.frame), y: 50);
+        self.addChild(playAgainButton)
+    }
+    
+    override func touchesBegan(touches: Set <NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            
+            // if the user touches the play again button
+            if nodeAtPoint(location) == playAgainButton {
+                
+                // create a new scene
+                let skView = self.view as SKView!
+                var scene = BoardScene(size: skView.bounds.size)
+                
+                // go to the new scene
+                skView.presentScene(scene)
+            }
+        }
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
