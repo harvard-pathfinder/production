59e95ca337b9c6f302d01c8a6c022c71b36d0749
Sean Coleman <seancoleman@college.harvard.edu>
2015-04-29 14:14:01 -0400

    gameOver Label bounce animation

--- pathfinder/GameOverScene.swift
+++ pathfinder/GameOverScene.swift
@@ -18,7 +18,20 @@ class GameOverScene: SKScene {
         gameOver.text = "Game Over"
         gameOver.fontSize = 50
         gameOver.fontColor = UIColor.redColor()
-        gameOver.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        gameOver.position = CGPoint(x:CGRectGetMidX(self.frame), y:(CGRectGetMidY(self.frame) + 350));
+        
+        // set up the physical world to make GameOver node bounce
+        self.physicsWorld.gravity = CGVectorMake(0, -7)
+        // make it stop half way
+        let physicsBody = SKPhysicsBody (edgeLoopFromRect: CGRect(x: 0,y: 200,
+            width: self.frame.size.width,height: self.frame.size.height))
+        self.physicsBody = physicsBody
+        
+        gameOver.physicsBody = SKPhysicsBody(circleOfRadius: gameOver.frame.size.width/2)
+        gameOver.physicsBody!.friction = 0.3
+        gameOver.physicsBody!.restitution = 0.8
+        gameOver.physicsBody!.mass = 0.3
+        
         playAgainButton.text = "Play Again";
         playAgainButton.fontSize = 20
         playAgainButton.position = CGPoint(x:CGRectGetMidX(self.frame), y: 50);
